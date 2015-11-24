b49d841d36828fa1136b36da3922b25236c0d533
chenxianjun <1004178387@qq.com>
2015-11-24 23:36:13 +0800

    搜索
    搜素

--- src/main/webapp/scripts/controllers/dbController.js
+++ src/main/webapp/scripts/controllers/dbController.js
@@ -4,6 +4,13 @@
 angular.module('ccloomi')
     .controller('dbCtrl',['$scope','$http','S_vis', function ($scope,$http,S_vis) {
         var options={};
+        $scope['info_onfocus']=false;
+        $scope.infoOnfocus= function () {
+            $scope['info_onfocus']=true;
+        };
+        $scope.infoOnblur= function () {
+            $scope['info_onfocus']=false;
+        }
         $http.post('db/asVisNetwork.json').success(function (data) {
             S_vis.network(document.getElementById('network'),options,data,$scope);
             $http.post('db/c2c.json').success(function (data) {
