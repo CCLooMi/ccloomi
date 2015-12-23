915927486f2a469b9c8ad73e2bd3ef1ef3b9627a
chenxianjun <1004178387@qq.com>
2015-12-23 22:43:34 +0800

    产品管理
    产品管理

---
+++ src/main/java/com/ccloomi/web/projManager/service/package-info.java
@@ -0,0 +1,8 @@
+/**© 2015-2015 CCLooMi.Inc Copyright
+ * 类    名：package-info
+ * 类 描 述：
+ * 作    者：Chenxj
+ * 邮    箱：chenios@foxmail.com
+ * 日    期：2015年12月23日-下午10:38:53
+ */
+package com.ccloomi.web.projManager.service;
