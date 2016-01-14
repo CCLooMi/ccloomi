76b5a17733476daaf4b8a748e3642fef8a3c8853
chenxianjun <1004178387@qq.com>
2016-01-14 20:48:38 +0800

    删除无用信息
    删除无用信息

--- src/main/java/com/ccloomi/web/fileUpload/server/UploadServer.java
+++ src/main/java/com/ccloomi/web/fileUpload/server/UploadServer.java
@@ -9,11 +9,13 @@ import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.web.socket.server.standard.SpringConfigurator;
 
-/**深圳市设计同道技术有限公司
+/**
+ * © 2015-2016 CCLooMi.Inc Copyright
  * 类    名：UploadServer
- * 类描述：
+ * 类 描 述：
  * 作    者：Chenxj
- * 日    期：2016年1月14日-下午1:49:45
+ * 邮    箱：chenios@foxmail.com
+ * 日    期：2016年1月14日-下午8:47:34
  */
 @ServerEndpoint(value="/springSocket/fileup",configurator=SpringConfigurator.class)
 public class UploadServer {
--- src/main/java/com/ccloomi/web/fileUpload/server/package-info.java
+++ src/main/java/com/ccloomi/web/fileUpload/server/package-info.java
@@ -1,7 +1,8 @@
-/**深圳市设计同道技术有限公司
+/**© 2015-2016 CCLooMi.Inc Copyright
  * 类    名：package-info
- * 类描述：
+ * 类 描 述：
  * 作    者：Chenxj
- * 日    期：2016年1月14日-下午2:02:39
+ * 邮    箱：chenios@foxmail.com
+ * 日    期：2016年1月14日-下午8:47:28
  */
 package com.ccloomi.web.fileUpload.server;
