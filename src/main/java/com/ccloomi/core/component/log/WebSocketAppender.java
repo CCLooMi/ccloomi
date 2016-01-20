bb673f69d6730aade21dd5b7d3aaae6c72a227c7
chenxianjun <1004178387@qq.com>
2016-01-20 23:36:24 +0800

    logback自定义appender
    logback自定义appender

---
+++ src/main/java/com/ccloomi/core/component/log/WebSocketAppender.java
@@ -0,0 +1,69 @@
+package com.ccloomi.core.component.log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.web.socket.server.standard.SpringConfigurator;
+
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+
+/**© 2015-2016 CCLooMi.Inc Copyright
+ * 类    名：WebSocketAppender
+ * 类 描 述：
+ * 作    者：Chenxj
+ * 邮    箱：chenios@foxmail.com
+ * 日    期：2016年1月20日-下午10:52:50
+ */
+@ServerEndpoint(value="/socket/log",configurator=SpringConfigurator.class)
+public class WebSocketAppender extends AppenderBase<ILoggingEvent>{
+	protected static List<WebSocketAppender>appenders=new ArrayList<>();
+	private PatternLayoutEncoder encoder;
+	private Session session;
+	/**获取 appenders*/
+	public static List<WebSocketAppender> getAppenders() {
+		return appenders;
+	}
+	/**设置 appenders*/
+	public static void setAppenders(List<WebSocketAppender> appenders) {
+		WebSocketAppender.appenders = appenders;
+	}
+	/**获取 encoder*/
+	public PatternLayoutEncoder getEncoder() {
+		return encoder;
+	}
+	/**设置 encoder*/
+	public void setEncoder(PatternLayoutEncoder encoder) {
+		this.encoder = encoder;
+	}
+	/**获取 session*/
+	public Session getSession() {
+		return session;
+	}
+	/**设置 session*/
+	public void setSession(Session session) {
+		this.session = session;
+	}
+	public void sendMessage(String message){
+		try {this.session.getBasicRemote().sendText(message);}
+		catch (IOException e) {}
+	}
+	@OnOpen
+	public void onOpen(Session session){
+		this.session=session;
+		WebSocketAppender.appenders.add(this);
+	}
+	@Override
+	protected void append(ILoggingEvent eventObject) {
+		for(WebSocketAppender appender:WebSocketAppender.appenders){
+			appender.sendMessage(this.encoder.getLayout().doLayout(eventObject));
+		}
+	}
+
+}
