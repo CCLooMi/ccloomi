636c3e47b240bc5b48aea03d05b3e8fe1735f11d
chenxianjun <1004178387@qq.com>
2016-03-20 11:01:47 +0800

    bug fix
    bug fix

--- src/main/webapp/js/bootstrap-ckeditor-modal-fix.js
+++ src/main/webapp/js/bootstrap-ckeditor-modal-fix.js
@@ -7,8 +7,9 @@ $.fn.modal.Constructor.prototype.enforceFocus = function () {
         var $parent = $(e.target.parentNode);
         if ($modalElement[0] !== e.target && !$modalElement.has(e.target).length
                 // add whatever conditions you need here:
-            &&
-            !$parent.hasClass('cke_dialog_ui_input_select') && !$parent.hasClass('cke_dialog_ui_input_text')) {
+            && !$parent.hasClass('cke_dialog_ui_input_select')
+            && !$parent.hasClass('cke_dialog_ui_input_text')
+            && !$parent.hasClass('cke_dialog_ui_input_textarea')) {
             $modalElement.focus()
         }
     })
