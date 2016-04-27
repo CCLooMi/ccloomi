041a1fab0f820b1eb40d950dcf6090e04e5dc810
CCLooMi <1004178387@qq.com>
2016-04-27 18:02:13 +0800

    从数据库加载模板
    从数据库加载模板

---
+++ src/main/java/com/ccloomi/web/system/freemarker/DBTemplateLoader.java
@@ -0,0 +1,38 @@
+package com.ccloomi.web.system.freemarker;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import com.ccloomi.web.system.constant.TemplateConstant;
+
+import freemarker.cache.TemplateLoader;
+/**
+ * 类名：DBTemplateLoader
+ * 描述：
+ * 作者： Chenxj
+ * 日期：2016年4月27日 - 下午5:39:42
+ */
+public class DBTemplateLoader implements TemplateLoader{
+
+	@Override
+	public Object findTemplateSource(String name) throws IOException {
+		return TemplateConstant.getTemplateConstantMap().get(name);
+	}
+
+	@Override
+	public long getLastModified(Object templateSource) {
+		return -1;
+	}
+
+	@Override
+	public Reader getReader(Object templateSource, String encoding) throws IOException {
+		return new StringReader((String) templateSource);
+	}
+
+	@Override
+	public void closeTemplateSource(Object templateSource) throws IOException {
+		
+	}
+
+}
---
+++ src/main/java/com/ccloomi/web/system/freemarker/package-info.java
@@ -0,0 +1,6 @@
+/**类名：package-info
+ * 描述：
+ * 作者： Chenxj
+ * 日期：2016年4月27日 - 下午6:01:28
+ */
+package com.ccloomi.web.system.freemarker;
