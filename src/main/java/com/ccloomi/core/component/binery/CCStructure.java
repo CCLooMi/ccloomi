3ce3e48fa09a00e2c7186483545256485205b6d0
chenxianjun <1004178387@qq.com>
2016-06-15 00:00:36 +0800

    结构体
    结构体

---
+++ src/main/java/com/ccloomi/core/component/binery/CCStructure.java
@@ -0,0 +1,12 @@
+package com.ccloomi.core.component.binery;
+
+/**© 2015-2016 CCLooMi.Inc Copyright
+ * 类    名：CCStructure
+ * 类 描 述：
+ * 作    者：Chenxj
+ * 邮    箱：chenios@foxmail.com
+ * 日    期：2016年6月14日-下午10:12:07
+ */
+public abstract class CCStructure {
+
+}
